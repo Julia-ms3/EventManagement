e3e77cc48e2bb900947863c50195759fa65968e9
Julia Matviychuk <Matviychukjulia03@gmail.com>
2025-05-28 00:01:11 +0300

    implemented models and added startapp users

--- event_manager/events/models.py
+++ event_manager/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    organizer = models.CharField(max_length=150)
